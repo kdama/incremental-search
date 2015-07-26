86159843032f30906943cc5f7a36e3663489f5fe
kdama <kai.kodama@outlook.com>
2015-07-26 22:28:28 +0900

    Update incremental-search.js

--- js/incremental-search.js
+++ js/incremental-search.js
@@ -3,98 +3,122 @@
 var $  = root.$;
 var ko = root.ko;
 
-/* Model */
+/* Collections */
 
 var Property = function( key, value ) {
-  var self = this;
-
-  self.key = ko.observable( key );
-  self.value = ko.observable( value );
+  this.key = ko.observable( key );
+  this.value = ko.observable( value );
 };
 
 var Item = function() {
-  var self = this;
+  this.properties = ko.observableArray( [] );
+};
 
-  self.properties = ko.observableArray( [] );
+Item.prototype.includes = function( keyword ) {
+  return this.properties().some( function( property ) {
+    return property.value().indexOf( keyword ) !== -1;
+  } );
+};
 
-  self.includes = function( keyword ) {
-    return self.properties().some( function( property ) {
-      return property.value().indexOf( keyword ) !== -1;
-    } );
-  };
+/* Model */
+
+var Model = function() {
+  this.items = ko.observableArray();
+  this.errorInfo = ko.observable();
+};
+
+var AjaxErrorInfo = function( xhr, status, error ) {
+  this.xhr = ko.observable( xhr );
+  this.status = ko.observable( status );
+  this.error = ko.observable( error );
+};
+
+Model.prototype.loadUrl = function( url ) {
+  $.ajax( url, {
+    type: "GET",
+    dataType: "jsonp",
+    context: this,
+  } ).done( function( data ) {
+    this.items(this._itemsFromGoogleSheetsJson( data ));
+    this.errorInfo( null );
+  } ).fail( function( xhr, status, error ) {
+    this.errorInfo( new AjaxErrorInfo( xhr, status, error ) );
+  } );
+};
+
+Model.prototype._itemsFromGoogleSheetsJson = function( data ) {
+  /*
+   *  Name   Age
+   *  ----------
+   *  Alice  20
+   *  Bob    30
+   *
+   *      |
+   *      v
+   *
+   *  [
+   *    { properties: [ { key: "Name", value: "Alice" }, { key: "Age", value: "20" } ] },
+   *    { properties: [ { key: "Name", value: "Bob"   }, { key: "Age", value: "30" } ] }
+   *  ]
+   *
+   */
+
+  var sheet = data.feed.entry;
+  var items = [];
+  var columnLabels = [];
+
+  Object.keys( sheet ).forEach( function( index ) {
+    var cell = sheet[ index ].gs$cell;
+    var cellCol = cell.col - 1; // col indices start at 1.
+    var cellRow = cell.row - 2; // row indices start at 2.
+    var cellData = cell.$t;
+
+    var property;
+
+    if ( cellRow === "-1" ) {
+      // this cell is a column label.
+      columnLabels[ cellCol ] = cellData;
+    } else {
+      items[ cellRow ] = items[ cellRow ] || new Item();
+      property = new Property( columnLabels[ cellCol ], cellData );
+
+      items[ cellRow ].properties.push( property );
+    }
+  });
+
+  return items;
 };
 
 /* ViewModel */
 
-var ViewModel = function( url ) {
-  var self = this;
+var ViewModel = function( url, model ) {
+  this.url = ko.observable( url );
+  this.keyword = ko.observable();
 
-  self.url = ko.observable( url );
-  self.keyword = ko.observable();
-  self.items = ko.observableArray();
-  self.errormsg = ko.observable();
+  this.model = ko.observable( model );
 
-  self.loadUrl = function() {
-    $.ajax( {
-      type: "GET",
-      url: self.url(),
-      dataType: "jsonp",
-      success: function( data ) {
-        self.items( self.itemsFromGoogleSheetsJson( data ) );
-        self.errormsg( null );
-      },
-      error: function( xhr, status, error ) {
-        self.errormsg( status + " " + xhr.status + ": " + error );
-      }
-    } );
-  };
+  this.items = ko.pureComputed( function() {
+    return this.model().items();
+  }, this );
 
-  self.itemsFromGoogleSheetsJson = function( data ) {
-    /*
-     *  Name   Age
-     *  ----------
-     *  Alice  20
-     *  Bob    30
-     *
-     *      |
-     *      v
-     *
-     *  [
-     *    { properties: [ { key: "Name", value: "Alice" }, { key: "Age", value: "20" } ] },
-     *    { properties: [ { key: "Name", value: "Bob"   }, { key: "Age", value: "30" } ] }
-     *  ]
-     *
-     */
-
-    var sheet = data.feed.entry;
-    var items = [];
-    var columns = [];
-
-    Object.keys( sheet ).forEach( function( index ) {
-      var cell = sheet[ index ].gs$cell;
-      var cellCol = cell.col - 1; // col indices start at 1.
-      var cellRow = cell.row - 2; // row indices start at 2.
-      var cellData = cell.$t;
-
-      if ( cellRow === "-1" ) {
-        // this cell is a column label.
-        columns[ cellCol ] = cellData;
-      } else {
-        items[ cellRow ] = items[ cellRow ] || new Item();
-        var property = new Property( columns[ cellCol ], cellData );
-        items[ cellRow ].properties.push( property );
-      }
-    });
-
-    return items;
-  };
-
-  self.matches = function( item ) {
-    // if keyword is empty, show all data.
-    return !self.keyword() || item.includes( self.keyword() );
-  };
+  this.errormsg = ko.pureComputed( function() {
+    var e = this.model().errorInfo();
+    if ( !e ) {
+      return null;
+    }
+    return e.xhr().status + " " + e.status() + ": " + e.error();
+  }, this );
 };
 
-ko.applyBindings( new ViewModel( "https://spreadsheets.google.com/feeds/cells/1NH9rvVIudYRMMU4ETmRNdiTJQR36xCVYviVWjTEj5pM/1/public/values?alt=json" ) );
+ViewModel.prototype.loadUrl = function() {
+  this.model().loadUrl( this.url() );
+};
+
+ViewModel.prototype.matches = function( item ) {
+  // if keyword is empty, show all data.
+  return !this.keyword() || item.includes( this.keyword() );
+};
+
+ko.applyBindings( new ViewModel( "https://spreadsheets.google.com/feeds/cells/1NH9rvVIudYRMMU4ETmRNdiTJQR36xCVYviVWjTEj5pM/1/public/values?alt=json", new Model() ) );
 
 } )( window );
